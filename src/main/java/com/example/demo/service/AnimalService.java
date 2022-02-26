a024df679f8d0a3630571a7fb26b699842ab6e44
vic90909 <vicmitoi@gmail.com>
2022-02-26 15:59:32 +0200

    Added animal service

---
+++ src/main/java/com/example/demo/service/AnimalService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimalService {
+
+    public String showAnimal(String name){
+        return name;
+    }
+}
